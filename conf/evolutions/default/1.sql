4f7c18e471c6ad9427228884b0d5057eabbced0e
mcuthbert <mgcuthbert@hotmail.com>
2016-08-26 15:29:39 -0700

    Adding ability to allow all users to be super users through the config file.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -412,11 +412,11 @@ $$
 LANGUAGE plpgsql VOLATILE;;
 
 -- Insert the default root, used for migration and those using the old API
---INSERT INTO projects (id, name, description) VALUES (0, 'SuperRootProject', 'Root Project for super users.');
---INSERT INTO groups(id, project_id, name, group_type)  VALUES (-999, 0, 'SUPERUSERS', -1);
---INSERT INTO users(id, osm_id, osm_created, name, oauth_token, oauth_secret, theme)
-    --VALUES (-999, -999, NOW(), 'SuperUser', '', '', 'skin-black');
---INSERT INTO user_groups (osm_user_id, group_id) VALUES (-999, -999);
+INSERT INTO projects (id, name, description) VALUES (0, 'SuperRootProject', 'Root Project for super users.');
+INSERT INTO groups(id, project_id, name, group_type)  VALUES (-999, 0, 'SUPERUSERS', -1);
+INSERT INTO users(id, osm_id, osm_created, name, oauth_token, oauth_secret, theme)
+    VALUES (-999, -999, NOW(), 'SuperUser', '', '', 0);
+INSERT INTO user_groups (osm_user_id, group_id) VALUES (-999, -999);
 
 # --- !Downs
 --DROP FUNCTION IF EXISTS create_index_if_not_exists(t_name text, i_name text, index_sql text, unq boolean);
