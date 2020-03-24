097ead3005f38f4d632fd6572f80ed706cb69372
mcuthbert <mcuthbert@apple.com>
2020-03-24 12:14:59 -0700

    Fixing port for database

--- test/org/maproulette/utils/TestDatabase.scala
+++ test/org/maproulette/utils/TestDatabase.scala
@@ -30,7 +30,7 @@ import play.api.libs.oauth.RequestToken
 trait TestDatabase extends PlaySpec with BeforeAndAfterAll with MockitoSugar {
   val application = GuiceApplicationBuilder()
     .configure(
-      "db.default.url"      -> "jdbc:postgresql://localhost:5433/mr_test",
+      "db.default.url"      -> "jdbc:postgresql://localhost:5432/mr_test",
       "db.default.username" -> "osm",
       "db.default.password" -> "osm",
       "db.default.logSql"   -> false
