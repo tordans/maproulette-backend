ab9e62836d7d83f50682056a303b86cb661302fe
Kelli Rotstan <kelli@rotstan.org>
2021-02-08 09:48:47 -0800

    Fix when retrieving a tag by name it needs a tag type
    When retrieving a tag by name it must have a tag type as there
    can be multiple tags of the same name but of different types (challenges/tasks)

--- app/org/maproulette/framework/graphql/schemas/TagSchema.scala
+++ app/org/maproulette/framework/graphql/schemas/TagSchema.scala
@@ -54,7 +54,10 @@ class TagSchema @Inject() (override val service: TagService)
       fieldType = OptionType(TagType),
       arguments = MRSchema.nameArg :: TagSchema.parentArg :: Nil,
       resolve = context =>
-        this.service.retrieveByName(context.arg(MRSchema.nameArg), context.arg(TagSchema.parentArg))
+        this.service.retrieveByName(
+          context.arg(MRSchema.nameArg),
+          context.arg(TagSchema.tagTypeArg),
+          context.arg(TagSchema.parentArg))
     ),
     Field(
       name = "keywordsByTask",
