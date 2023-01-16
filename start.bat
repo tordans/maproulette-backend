fca024e5fa3c832cedd0a17e1f928451626d33ef
Lucas Burson <ljdelight@gmail.com>
2023-01-16 02:06:04 -0600

    Remove old files (#1011)

--- start.bat
+++
@@ -1,9 +0,0 @@
-set "MR_APPLICATION_SECRET=lame-ducks-forever"
-set "MR_DATABASE_URL=jdbc:postgresql://localhost:5432/mp_dev?user=postgres&password=osm"
-set "MR_OSM_SERVER=http://api06.dev.openstreetmap.org"
-set "MR_OAUTH_CONSUMER_KEY=8kOhMqcXVAmD3ZoBdjNgcmd93ErztPnIPpc0xjzM"
-set "MR_OAUTH_CONSUMER_SECRET=bAesEwMMwZO6wMgFMqAudZ12N5caQvAAsx3FbkFc"
-set "MR_SUPER_KEY=test"
-set "MR_SUPER_ACCOUNTS=437"
-
-activator run
