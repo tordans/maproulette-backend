184a0f1b9c84cc2c902fd67ad3a33fa785d90f06
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2024-08-12 14:05:03 -0500

    fix stale review_started_at column (#1138)
    * fix stale review_started_at column
    
    * run `sbt scalafmt`

--- app/org/maproulette/framework/repository/TaskReviewRepository.scala
+++ app/org/maproulette/framework/repository/TaskReviewRepository.scala
@@ -83,21 +83,11 @@ class TaskReviewRepository @Inject() (
         .executeUpdate()
 
       for (task <- taskList) {
-        var querystring =
-          """UPDATE task_review SET review_claimed_by = {userId}, review_claimed_at = NOW(), review_started_at = NOW()
-                    WHERE task_id = {taskId} AND review_claimed_at IS NULL"""
-
-        //don't reset review_started_at if task has already been reviewed
-        if (!task.review.reviewedAt.isEmpty) {
-          querystring =
-            """UPDATE task_review SET review_claimed_by = {userId}, review_claimed_at = NOW()
-                    WHERE task_id = {taskId} AND review_claimed_at IS NULL"""
-        }
-
         Query
           .simple(List())
           .build(
-            querystring
+            """UPDATE task_review SET review_claimed_by = {userId}, review_claimed_at = NOW(), review_started_at = NOW()
+                    WHERE task_id = {taskId} AND review_claimed_at IS NULL"""
           )
           .on(Symbol("taskId") -> task.id, Symbol("userId") -> user.id)
           .executeUpdate()
